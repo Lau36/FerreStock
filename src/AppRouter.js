c9dc15c34a3667536f341197c915c7426b2ad9b3
Lau36 <laura.jaimes@correounivalle.edu.co>
2024-04-16 15:30:37 -0500

    AppRoute

---
+++ src/AppRouter.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;
