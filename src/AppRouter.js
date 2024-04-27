379b8a179fa956498f87379d2f12e26f1b208553
Lau36 <laura.jaimes@correounivalle.edu.co>
2024-04-27 09:49:08 -0500

    Changes login styles

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import Register from "./Register/register";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Register from "./register/register";
 import Login from "./Login/Login.jsx";
 import Home from "./Home/Index";
 
 function AppRouter() {
-  const isLoggedIn = window.localStorage.getItem('token'); // Modificado para usar el token
+  const isLoggedIn = window.localStorage.getItem("token"); // Modificado para usar el token
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={isLoggedIn ? <Navigate to="/Home" /> : <Login />} />
+        <Route
+          path="/"
+          element={isLoggedIn ? <Navigate to="/Home" /> : <Login />}
+        />
         <Route path="/Registro" element={<Register />} />
         <Route path="/Home" element={<Home />} />
       </Routes>
