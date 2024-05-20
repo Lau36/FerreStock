44d0508c19ee20dfbb957206157787a08fdde647
Lau36 <laura.jaimes@correounivalle.edu.co>
2024-05-20 08:38:49 -0500

    addOrders working

--- src/CreateOrders/Index.js
+++ src/CreateOrders/Index.js
@@ -112,6 +112,7 @@ function CreateOrders() {
       const updatedProducts = [...items, newItem];
       setRows(updatedRows);
       setItems(updatedProducts);
+      setOpen(false);
 
       console.log(updatedRows);
 
